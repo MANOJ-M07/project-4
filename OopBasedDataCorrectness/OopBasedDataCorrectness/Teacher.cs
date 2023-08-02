8321544faae88263e61f398e77653fd6dfe50d21
MANOJ-M07 <manojmoorthy01@gmail.com>
2023-08-02 21:59:40 +0530

    initial commit

---
+++ OopBasedDataCorrectness/OopBasedDataCorrectness/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OopBasedDataCorrectness
+{
+    internal class Teacher
+    {
+        public string Name { get; set; }
+        public string ClassAndSection { get; set; }
+        public List<Subject> SubjectsTaught { get; set; }
+
+        public Teacher()
+        {
+            SubjectsTaught = new List<Subject>();
+        }
+    }
+}
